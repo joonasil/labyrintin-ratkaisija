dbb4e56df51591751cbb814fa43941578c3dfe23
joonasil <joonas.ilvonen@outlook.com>
2016-09-18 14:49:19 +0300

    Ohjelman rakenteen päivittelyä

---
+++ mazesolver/src/main/java/fi/joonasil/mazesolver/util/Tree.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fi.joonasil.mazesolver.util;
+
+import java.util.List;
+
+/**
+ *
+ * @author Joonas
+ */
+public class Tree<T> {
+    private T data;
+    private Tree<T> parent;
+    private List<Tree<T>> children;
+    
+    
+}
